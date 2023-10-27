3e2674636c2f8dbc23f9fa8e2b16c6072cd3821c
zhangyong511 <56580018+zhangyong511@users.noreply.github.com>
2023-10-27 14:18:56 +0800

    Update distribute_train.py

--- distribute_train.py
+++ distribute_train.py
@@ -28,10 +28,10 @@ import json
 def get_args():
     parser = argparse.ArgumentParser(description='获得分布式训练参数')
     parser.add_argument('--single_gpu_batch_size', '-s', help='batch size for single gpu', default=15, type=int)
-    parser.add_argument('--training_epoch', '-te', help='training epoch', default=2, type=int)  # 训练epoch
+    parser.add_argument('--training_epoch', '-te', help='training epoch', default=11, type=int)  # 训练epoch
     parser.add_argument('--log_step', '-ls', help='log step', default=10, type=int)
-    parser.add_argument('--dataset_dirs', '-d', help='dataset path', default="/mnt/ve_share2/zy/language_table_embedding/language_table_use/1.0.0")
-    parser.add_argument('--learning_rate', '-lr', help='learning rate', default=0.00001, type=float)  # 学习率
+    parser.add_argument('--dataset_dirs', '-d', help='dataset path', default="/mnt/ve_share2/zy/language_table_sim_use/language_table_use/1.0.0")
+    parser.add_argument('--learning_rate', '-lr', help='learning rate', default=0.00005, type=float)  # 学习率  0.00001
     parser.add_argument('--vocab_size', '-vs', help='vocab size for discretization', default=256, type=int)  # 离散词典大小
     parser.add_argument('--dataset_episode_num', '-den', help='训练数据量', default=10000, type=int)
     parser.add_argument('--loaded_checkpoints_dir', '-lcd', help='模型加载目录', default="/mnt/ve_share2/zy/robotics_transformer/save_checkpoint", type=str)
@@ -98,8 +98,8 @@ def create_model(args):
     action_spec.terminate_episode = tensor_spec.BoundedTensorSpec(
         (2,), dtype=tf.int32, minimum=0, maximum=1, name='terminate_episode')
 
-    action_spec.effector_target_translation = tensor_spec.BoundedTensorSpec(
-        (2,), dtype=tf.float32, minimum=-0.3, maximum=0.6, name='effector_target_translation')
+    action_spec.action = tensor_spec.BoundedTensorSpec(
+        (2,), dtype=tf.float32, minimum=-0.03, maximum=0.03, name='action')
 
     network = transformer_network.TransformerNetwork(
         input_tensor_spec=state_spec,
@@ -114,7 +114,7 @@ def create_model(args):
         time_sequence_length=time_sequence_length,
         crop_size=236,
         use_token_learner=True,
-        action_order=['terminate_episode', 'effector_target_translation'])
+        action_order=['terminate_episode', 'action'])
     return network
 
 
@@ -152,13 +152,11 @@ if __name__ == '__main__':
     # )
     # mirrored_strategy = tf.distribute.MultiWorkerMirroredStrategy(
     #     communication_options=options)
-    mirrored_strategy = tf.distribute.MirroredStrategy()
+    
+    mirrored_strategy = tf.distribute.MirroredStrategy()  
     global_batch_size = args.single_gpu_batch_size * mirrored_strategy.num_replicas_in_sync
-
     global_learning_rate = args.learning_rate * global_batch_size
-    # global_batch_size = args.single_gpu_batch_size * mirrored_strategy.num_replicas_in_sync
 
-    # global_learning_rate = args.learning_rate * global_batch_size
     logdir = os.path.join(args.checkpoints_saved_dir, 'logs', datetime.now().strftime("%Y%m%d-%H%M%S"))
     summary_writer = tf.summary.create_file_writer(logdir)
 
@@ -178,10 +176,10 @@ if __name__ == '__main__':
 
         ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, model=network)
         # if tf.train.latest_checkpoint(args.loaded_checkpoints_dir):
-        print(tf.train.latest_checkpoint(args.loaded_checkpoints_dir))
-        ckpt.restore(tf.train.latest_checkpoint(args.loaded_checkpoints_dir))
+        # print(tf.train.latest_checkpoint(args.loaded_checkpoints_dir))
+        # ckpt.restore(tf.train.latest_checkpoint(args.loaded_checkpoints_dir))
         
-        print("从 %s 恢复模型" % (args.loaded_checkpoints_dir))
+        # print("从 %s 恢复模型" % (args.loaded_checkpoints_dir))
 
         current_step = ckpt.step.numpy()
         print("开始训练")
@@ -196,7 +194,7 @@ if __name__ == '__main__':
                 model(observation_batch, step_type=None, network_state=network_state, training=True)
                 loss = tf.reduce_mean(model.get_actor_loss())
                 gradients = tape.gradient(loss, model.trainable_variables,
-                                          unconnected_gradients=tf.UnconnectedGradients.ZERO)
+                                            unconnected_gradients=tf.UnconnectedGradients.ZERO)
                 optimizer.apply_gradients(grads_and_vars=zip(gradients, model.trainable_variables))
                 logging_info = model.get_aux_info()
                 return loss, logging_info
@@ -204,7 +202,7 @@ if __name__ == '__main__':
 
         # action_order = network._action_order
         action_order =network._action_tokenizer.action_order
-      
+        
         for epoch in range(1, args.training_epoch):
             total_loss = 0.0
             step = 0
@@ -216,7 +214,7 @@ if __name__ == '__main__':
                 per_replica_losses, logging_info = mirrored_strategy.run(
                     train_one_step, args=(network, train_observation, train_labels, network_state, optimizer))
                 step = step + 1
-                mean_loss = mirrored_strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_losses, axis=None)
+                mean_loss = mirrored_strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_losses, axis=None)   
                 total_loss = total_loss + mean_loss
                 ckpt.step.assign_add(1)
                 # print('训练1个step耗时: %s s' % (10))
